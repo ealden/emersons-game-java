f6eb3c3159282059992c6ce91a4ad1323b9a1050
Ealden Esto E. Escañan <ealden@escanan.com>
2021-04-24 20:06:01 +0800

    Import Emerson's Game web assets
    Mock GET /api/races and GET /api/settings endpoints until we cam
    implement them using Servlets.

---
+++ src/main/webapp/js/index.js
@@ -0,0 +1,83 @@
+const EmersonsGame = {
+  data () {
+    return {
+      finishLine: 0,
+      racers: [],
+      message: null,
+      currentRacer: null,
+      allCrashed: false,
+      over: false,
+      testMode: false,
+      roll: 0,
+      processing: false
+    }
+  },
+  computed: {
+    track: function () {
+      return (this.finishLine - 1)
+    },
+    ready: function () {
+      return (this.racers.length > 0)
+    }
+  },
+  methods: {
+    fetchRace: function () {
+      axios
+        .get('/api/races')
+        .then(response => {
+          this.racers = response.data.racers
+          this.finishLine = response.data.finishLine
+          this.message = response.data.message
+          this.currentRacer = response.data.currentRacer
+          this.allCrashed = response.data.allCrashed
+          this.over = response.data.over
+
+          this.roll = 0
+
+          this.processing = false
+        })
+    },
+    fetchSettings: function () {
+      axios
+        .get('/api/settings')
+        .then(response => {
+          this.testMode = response.data.testMode
+
+          this.processing = false
+        })
+    },
+    rollNormalSpeed: function (event) {
+      this.doRoll('NORMAL')
+    },
+    rollSuperSpeed: function (event) {
+      this.doRoll('SUPER')
+    },
+    doRoll: function (speedType) {
+      this.processing = true
+
+      axios
+        .post('/api/races/roll', {
+          'speedType': speedType,
+          'roll': this.roll
+        })
+        .then(response => {
+          this.fetchRace()
+        })
+    },
+    newRace: function () {
+      this.processing = true
+
+      axios
+        .post('/api/races/new', {})
+        .then(response => {
+          this.fetchRace()
+        })
+    }
+  },
+  mounted () {
+    this.fetchSettings()
+    this.fetchRace()
+  }
+}
+
+Vue.createApp(EmersonsGame).mount('#app')
