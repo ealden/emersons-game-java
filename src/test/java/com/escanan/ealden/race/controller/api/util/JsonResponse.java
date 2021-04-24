345e23ebbc4fee6a3c926fc1ffa91d9422116150
Ealden Esto E. Escañan <ealden@escanan.com>
2021-04-25 02:17:54 +0800

    JsonResponse util to load json files from src/test/resources/responses

---
+++ src/test/java/com/escanan/ealden/race/controller/api/util/JsonResponse.java
@@ -0,0 +1,17 @@
+package com.escanan.ealden.race.controller.api.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JsonResponse {
+    private static final String RESPONSES_DIR = "src/test/resources/responses";
+    private static final String JSON_EXTENSION = ".json";
+
+    public static String of(String url) throws IOException {
+        Path file = Paths.get(RESPONSES_DIR + url + JSON_EXTENSION);
+
+        return new String(Files.readAllBytes(file));
+    }
+}
