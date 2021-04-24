1c0c17127c5aef5a133e6b28ab1bca21296abea2
Ealden Esto E. Escañan <ealden@escanan.com>
2021-04-25 02:18:22 +0800

    jsonResponseOf() matcher

---
+++ src/test/java/com/escanan/ealden/race/controller/api/util/IsJsonResponseOf.java
@@ -0,0 +1,42 @@
+package com.escanan.ealden.race.controller.api.util;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+import java.io.IOException;
+
+public class IsJsonResponseOf extends TypeSafeMatcher<String> {
+    private static final String SPACE = " ";
+    private static final String NEW_LINE = "\n";
+    private static final String BLANK = "";
+
+    private String url;
+    private String json;
+
+    public IsJsonResponseOf(String url) throws IOException {
+        this.url = url;
+        this.json = JsonResponse.of(url);
+    }
+
+    @Override
+    protected boolean matchesSafely(String item) {
+        String expected = trimSpacesAndNewLines(json);
+        String actual = trimSpacesAndNewLines(item);
+
+        return expected.equals(actual);
+    }
+
+    private String trimSpacesAndNewLines(String input) {
+        return input.trim().replaceAll(SPACE, BLANK).replaceAll(NEW_LINE, BLANK);
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("response of " + url);
+    }
+
+    public static Matcher<String> jsonResponseOf(String url) throws IOException {
+        return new IsJsonResponseOf(url);
+    }
+}
