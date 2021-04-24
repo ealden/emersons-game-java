7effb483f432e3398b77cade2fc1114b4275ec04
Ealden Esto E. Escañan <ealden@escanan.com>
2021-04-25 02:31:56 +0800

    Import Emerson's Game SpeedType class
    Implementation should be the same across Java and Spring versions.

--- src/main/java/com/escanan/ealden/race/model/SpeedType.java
+++ src/main/java/com/escanan/ealden/race/model/SpeedType.java
@@ -1,5 +1,36 @@
 package com.escanan.ealden.race.model;
 
+import static java.lang.Math.max;
+
 public enum SpeedType {
-    NORMAL, SUPER
+    NORMAL(1, 2, 1, 0),
+    SUPER(0, 7, 0, 2);
+
+    private int prefix;
+    private int modulo;
+    private int offset;
+    private int damage;
+
+    SpeedType(int prefix, int modulo, int offset, int damage) {
+        this.prefix = prefix;
+        this.modulo = modulo;
+        this.offset = offset;
+        this.damage = damage;
+    }
+
+    public int calculate(int number) {
+        if (number <= 0) {
+            return 0;
+        }
+
+        return (((prefix + number) % modulo) + offset);
+    }
+
+    public int move(int number, int damage) {
+        return max((calculate(number) - damage), 0);
+    }
+
+    public int getDamage() {
+        return damage;
+    }
 }
