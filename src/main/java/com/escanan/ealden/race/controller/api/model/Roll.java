2ea81f9abe2cd7819cef4351b421d1ca8ded87c9
Ealden Esto E. Escañan <ealden@escanan.com>
2021-04-25 13:19:53 +0800

    Initial Roll value object

--- src/main/java/com/escanan/ealden/race/controller/api/model/Roll.java
+++ src/main/java/com/escanan/ealden/race/controller/api/model/Roll.java
@@ -1,6 +1,45 @@
 package com.escanan.ealden.race.controller.api.model;
 
+import com.escanan.ealden.race.model.SpeedType;
+
+import java.util.Map;
+
 public class Roll {
     public static final String ROLL_PARAM = "roll";
     public static final String SPEED_TYPE_PARAM = "speedType";
+
+    private Integer roll;
+    private SpeedType speedType;
+
+    public static Roll fromParameters(Map<String, String> parameters) {
+        Integer number = null;
+
+        if (parameters.containsKey(ROLL_PARAM)) {
+            number = Integer.parseInt(parameters.get(ROLL_PARAM));
+        }
+
+        SpeedType speedType = SpeedType.valueOf(parameters.get(SPEED_TYPE_PARAM));
+
+        Roll roll = new Roll();
+        roll.setRoll(number);
+        roll.setSpeedType(speedType);
+
+        return roll;
+    }
+
+    public Integer getRoll() {
+        return roll;
+    }
+
+    public SpeedType getSpeedType() {
+        return speedType;
+    }
+
+    public void setRoll(Integer roll) {
+        this.roll = roll;
+    }
+
+    public void setSpeedType(SpeedType speedType) {
+        this.speedType = speedType;
+    }
 }
---
+++ src/test/java/com/escanan/ealden/race/controller/api/model/RollTest.java
@@ -0,0 +1,37 @@
+package com.escanan.ealden.race.controller.api.model;
+
+import com.escanan.ealden.race.model.SpeedType;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.escanan.ealden.race.controller.api.model.Roll.ROLL_PARAM;
+import static com.escanan.ealden.race.controller.api.model.Roll.SPEED_TYPE_PARAM;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class RollTest {
+    @Test
+    public void fromParametersMustReturnRollModel() {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put(ROLL_PARAM, "1");
+        parameters.put(SPEED_TYPE_PARAM, "NORMAL");
+
+        Roll roll = Roll.fromParameters(parameters);
+
+        assertThat(roll.getRoll(), is(equalTo(1)));
+        assertThat(roll.getSpeedType(), is(equalTo(SpeedType.NORMAL)));
+    }
+
+    @Test
+    public void fromParametersMustReturnRollWithNullNumberIfParameterIsNotPresent() {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put(SPEED_TYPE_PARAM, "NORMAL");
+
+        Roll roll = Roll.fromParameters(parameters);
+
+        assertThat(roll.getRoll(), is(nullValue()));
+        assertThat(roll.getSpeedType(), is(equalTo(SpeedType.NORMAL)));
+    }
+}
