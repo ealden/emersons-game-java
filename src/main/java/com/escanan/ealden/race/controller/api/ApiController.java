e0bb789aa7ef0dd11f06765deb89b32172455b52
Ealden Esto E. Escañan <ealden@escanan.com>
2021-04-25 13:05:56 +0800

    Pull up jsonRequest()

--- src/main/java/com/escanan/ealden/race/controller/api/ApiController.java
+++ src/main/java/com/escanan/ealden/race/controller/api/ApiController.java
@@ -1,11 +1,16 @@
 package com.escanan.ealden.race.controller.api;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Type;
+import java.util.Map;
 
 public abstract class ApiController extends HttpServlet {
     private final Gson gson = new Gson();
@@ -19,5 +24,21 @@ public abstract class ApiController extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.print(responseBody);
         out.flush();
+    }
+
+    protected Map<String, String> jsonRequest(HttpServletRequest request) throws IOException {
+        BufferedReader reader = request.getReader();
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+
+        Type collectionType = new TypeToken<Map<String, String>>(){}.getType();
+        Map<String, String> parameters = new Gson().fromJson(sb.toString(), collectionType);
+
+        return parameters;
     }
 }
--- src/main/java/com/escanan/ealden/race/controller/api/RollController.java
+++ src/main/java/com/escanan/ealden/race/controller/api/RollController.java
@@ -4,15 +4,11 @@ import com.escanan.ealden.race.model.Race;
 import com.escanan.ealden.race.model.SpeedType;
 import com.escanan.ealden.race.service.RaceService;
 import com.escanan.ealden.race.service.impl.RaceServiceImpl;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.Map;
 
 @WebServlet(name = "Roll", urlPatterns = "/api/races/roll")
@@ -30,22 +26,6 @@ public class RollController extends ApiController {
         race.roll(roll, speedType);
 
         jsonResponse(race, response);
-    }
-
-    private Map<String, String> jsonRequest(HttpServletRequest request) throws IOException {
-        BufferedReader reader = request.getReader();
-
-        StringBuilder sb = new StringBuilder();
-        String line;
-
-        while ((line = reader.readLine()) != null) {
-            sb.append(line);
-        }
-
-        Type collectionType = new TypeToken<Map<String, String>>(){}.getType();
-        Map<String, String> parameters = new Gson().fromJson(sb.toString(), collectionType);
-
-        return parameters;
     }
 
     public void setRaceService(RaceService raceService) {
