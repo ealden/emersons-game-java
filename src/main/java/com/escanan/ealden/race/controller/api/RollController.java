b36449d8737dfc44ce0d03e55a9320029ba3979a
Ealden Esto E. Escañan <ealden@escanan.com>
2021-04-25 13:21:20 +0800

    Use Roll value object in controller

--- src/main/java/com/escanan/ealden/race/controller/api/RollController.java
+++ src/main/java/com/escanan/ealden/race/controller/api/RollController.java
@@ -1,7 +1,7 @@
 package com.escanan.ealden.race.controller.api;
 
+import com.escanan.ealden.race.controller.api.model.Roll;
 import com.escanan.ealden.race.model.Race;
-import com.escanan.ealden.race.model.SpeedType;
 import com.escanan.ealden.race.service.RaceService;
 import com.escanan.ealden.race.service.impl.RaceServiceImpl;
 
@@ -9,10 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
-
-import static com.escanan.ealden.race.controller.api.model.Roll.ROLL_PARAM;
-import static com.escanan.ealden.race.controller.api.model.Roll.SPEED_TYPE_PARAM;
 
 @WebServlet(name = "Roll", urlPatterns = "/api/races/roll")
 public class RollController extends ApiController {
@@ -20,13 +16,10 @@ public class RollController extends ApiController {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Map<String, String> parameters = jsonRequest(request);
-
-        int roll = Integer.parseInt(parameters.get(ROLL_PARAM));
-        SpeedType speedType = SpeedType.valueOf(parameters.get(SPEED_TYPE_PARAM));
+        Roll roll = Roll.fromParameters(jsonRequest(request));
 
         Race race = raceService.getCurrentRace();
-        race.roll(roll, speedType);
+        race.roll(roll.getRoll(), roll.getSpeedType());
 
         jsonResponse(race, response);
     }
