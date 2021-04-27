7eab032dd2ad8f9849ff07aa99a014e4a17927e4
Ealden Esto E. Escañan <ealden@escanan.com>
2021-04-28 00:41:20 +0800

    No need for Settings value object

--- src/main/java/com/escanan/ealden/race/controller/api/SettingsController.java
+++ src/main/java/com/escanan/ealden/race/controller/api/SettingsController.java
@@ -1,20 +1,23 @@
 package com.escanan.ealden.race.controller.api;
 
 import com.escanan.ealden.race.Configurations;
-import com.escanan.ealden.race.controller.api.model.Settings;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "Settings", urlPatterns = "/api/settings")
 public class SettingsController extends ApiController {
+    private static final String TEST_MODE_PARAM = "testMode";
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Settings settings = new Settings();
-        settings.setTestMode(Configurations.isTestMode());
+        Map<String, Boolean> json = new HashMap<>();
+        json.put(TEST_MODE_PARAM, Configurations.isTestMode());
 
-        renderJson(settings, response);
+        renderJson(json, response);
     }
 }
