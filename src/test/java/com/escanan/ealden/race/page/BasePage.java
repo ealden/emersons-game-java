7c165debebffbcf0412af25e85208146395d5590
Ealden Esto E. Escañan <ealden@escanan.com>
2021-05-15 11:32:17 +0800

    Pulled up common Page methods to BasePage

---
+++ src/test/java/com/escanan/ealden/race/page/BasePage.java
@@ -0,0 +1,80 @@
+package com.escanan.ealden.race.page;
+
+import com.escanan.ealden.race.model.Racer;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.CdpVersionFinder;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.ProtocolHandshake;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.OFF;
+import static org.openqa.selenium.support.ui.ExpectedConditions.invisibilityOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+public abstract class BasePage {
+    protected final WebDriver driver;
+
+    private final By testProcessing = By.id("test-processing");
+
+    public BasePage(boolean headless) {
+        reallySilenceSelenium();
+
+        driver = createDriver(headless);
+    }
+
+    private void reallySilenceSelenium() {
+        Logger.getLogger(ProtocolHandshake.class.getName()).setLevel(OFF);
+        Logger.getLogger(CdpVersionFinder.class.getName()).setLevel(OFF);
+    }
+
+    private WebDriver createDriver(boolean headless) {
+        WebDriverManager.edgedriver().setup();
+
+        EdgeOptions options = new EdgeOptions();
+        options.setHeadless(headless);
+
+        return new EdgeDriver(options);
+    }
+
+    public void close() {
+        driver.quit();
+    }
+
+    protected void click(By element) {
+        findElement(element).click();
+    }
+
+    protected void input(By element, int i) {
+        input(element, "" + i);
+    }
+
+    protected void input(By element, String keys) {
+        findElement(element).clear();
+        findElement(element).sendKeys(keys);
+    }
+
+    protected void waitUntilProcessingComplete() {
+        doWait().until(invisibilityOfElementLocated(testProcessing));
+    }
+
+    private WebDriverWait doWait() {
+        return new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    protected WebElement findElement(By by) {
+        return doWait().until(visibilityOfElementLocated(by));
+    }
+
+    protected WebElement findTestElement(Racer racer, String key) {
+        String testId = String.join("-", "test", "racer", racer.getId().toString(), key);
+
+        return findElement(By.id(testId));
+    }
+}
--- src/test/java/com/escanan/ealden/race/page/RacePage.java
+++ src/test/java/com/escanan/ealden/race/page/RacePage.java
@@ -2,31 +2,14 @@ package com.escanan.ealden.race.page;
 
 import com.escanan.ealden.race.model.Racer;
 import com.escanan.ealden.race.model.SpeedType;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.CdpVersionFinder;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.remote.ProtocolHandshake;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.logging.Logger;
 
 import static java.lang.Integer.parseInt;
-import static java.util.logging.Level.OFF;
-import static org.openqa.selenium.support.ui.ExpectedConditions.invisibilityOfElementLocated;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
-public class RacePage {
-    private static final String ROOT_URL = "http://localhost:8080/";
-
-    private final WebDriver driver;
+public class RacePage extends BasePage {
+    private static final String URL = "http://localhost:8080/";
 
     private final By testRoll = By.id("test-roll");
-    private final By testProcessing = By.id("test-processing");
 
     private final By raceControls = By.id("race-controls");
     private final By raceOver = By.id("race-over");
@@ -38,33 +21,13 @@ public class RacePage {
     private final By message = By.id("message");
 
     public RacePage(boolean headless) {
-        reallySilenceSelenium();
-
-        driver = createDriver(headless);
-    }
-
-    private void reallySilenceSelenium() {
-        Logger.getLogger(ProtocolHandshake.class.getName()).setLevel(OFF);
-        Logger.getLogger(CdpVersionFinder.class.getName()).setLevel(OFF);
+        super(headless);
     }
 
     public RacePage load() {
-        driver.navigate().to(ROOT_URL);
+        driver.navigate().to(URL);
 
         return this;
-    }
-
-    private WebDriver createDriver(boolean headless) {
-        WebDriverManager.edgedriver().setup();
-
-        EdgeOptions options = new EdgeOptions();
-        options.setHeadless(headless);
-
-        return new EdgeDriver(options);
-    }
-
-    public void close() {
-        driver.quit();
     }
 
     public RacePage roll(int roll, SpeedType speedType) {
@@ -89,19 +52,6 @@ public class RacePage {
         return this;
     }
 
-    private void click(By element) {
-        findElement(element).click();
-    }
-
-    private void input(By element, int i) {
-        input(element, "" + i);
-    }
-
-    private void input(By element, String keys) {
-        findElement(element).clear();
-        findElement(element).sendKeys(keys);
-    }
-
     public int getPositionOf(Racer racer) {
         return parseInt(findTestElement(racer, "position").getText());
     }
@@ -119,23 +69,5 @@ public class RacePage {
 
     public String getMessage() {
         return findElement(message).getText().trim();
-    }
-
-    private void waitUntilProcessingComplete() {
-        doWait().until(invisibilityOfElementLocated(testProcessing));
-    }
-
-    private WebDriverWait doWait() {
-        return new WebDriverWait(driver, Duration.ofSeconds(20));
-    }
-
-    private WebElement findElement(By by) {
-        return doWait().until(visibilityOfElementLocated(by));
-    }
-
-    private WebElement findTestElement(Racer racer, String key) {
-        String testId = String.join("-", "test", "racer", racer.getId().toString(), key);
-
-        return findElement(By.id(testId));
     }
 }
