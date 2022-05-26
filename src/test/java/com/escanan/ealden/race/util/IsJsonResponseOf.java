4b196c807eeeb30b697f9c035cb316dfc58036f0
Ealden Esto E. Escañan <ealden@escanan.com>
2022-05-27 01:58:57 +0800

    Convert JSON to Map then compare

--- src/test/java/com/escanan/ealden/race/util/IsJsonResponseOf.java
+++ src/test/java/com/escanan/ealden/race/util/IsJsonResponseOf.java
@@ -1,38 +1,31 @@
 package com.escanan.ealden.race.util;
 
+import com.google.gson.Gson;
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class IsJsonResponseOf extends TypeSafeMatcher<String> {
-    private static final String SPACE = " ";
-    private static final String LF = "\n";
-    private static final String CRLF = "\r\n";
-    private static final String BLANK = "";
-
-    private final String url;
     private final String json;
 
     public IsJsonResponseOf(String url) throws IOException {
-        this.url = url;
         this.json = JsonResponse.of(url);
     }
 
     @Override
-    protected boolean matchesSafely(String item) {
-        String expected = trimSpacesAndNewLines(json);
-        String actual = trimSpacesAndNewLines(item);
+    protected boolean matchesSafely(String input) {
+        Gson gson = new Gson();
+
+        Map expected = gson.fromJson(json, Map.class);
+        Map actual = gson.fromJson(input, Map.class);
 
         return expected.equals(actual);
     }
 
-    private String trimSpacesAndNewLines(String input) {
-        return input.trim().replaceAll(SPACE, BLANK).replaceAll(LF, BLANK).replaceAll(CRLF, BLANK);
-    }
-
     @Override
     public void describeTo(Description description) {
-        description.appendText("response of " + url);
+        description.appendText(json);
     }
 }
